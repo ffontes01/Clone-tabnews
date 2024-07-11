a4ff27ee72a45b44749a00e7c3829f5e5616766c
Felipe Fontes <49998254+ffontes01@users.noreply.github.com>
2024-07-11 03:34:44 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      Cuidado ao levar documentos para alguem, isso pode dar Casamento! 🤠 Se
+      gostou dessa mensagem dá uma risadinha 🌼🌼🌼
+    </h1>
+  );
+}
+
+export default Home;
